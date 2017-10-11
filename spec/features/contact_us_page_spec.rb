75a326bd608d6c968f4f5bc369617a81e871fa9b
chuk-chuk <yulia.v.litvin@gmail.com>
2017-10-12 00:20:58 +0100

    Scenario - user can not leave empty fields

--- spec/features/contact_us_page_spec.rb
+++ spec/features/contact_us_page_spec.rb
@@ -39,4 +39,19 @@ feature 'contact us page' do
     sleep 5
     expect(page).to have_content 'THANK YOU FOR YOUR MESSAGE.'
   end
+
+  scenario 'user can not leave empty fields' do
+    visit ('/')
+    first('.fusion-main-menu').click_link('CONTACT US')
+    fill_in 'Name', with: ''
+    fill_in 'Email', with: ''
+    fill_in 'Subject/Company/Project', with: 'This is not a required field'
+    fill_in 'How can we help you?', with: ''
+    click_button('send')
+    sleep 5
+    within('form') do
+        expect(page).to have_content('The field is required.', count: 3)
+    end
+    expect(page).to have_content 'PLEASE RE-SUBMIT WITH CORRECT INFORMATION'
+  end
 end
