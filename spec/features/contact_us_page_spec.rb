d030c3250724fc1b7638e39d29a6251b1b20cecc
chuk-chuk <yulia.v.litvin@gmail.com>
2017-10-11 22:46:05 +0100

    scenario - user can fill in a contact form

--- spec/features/contact_us_page_spec.rb
+++ spec/features/contact_us_page_spec.rb
@@ -9,9 +9,22 @@ feature 'contact us page' do
     expect(page).to have_content 'QAWorks have been acquired by ECS Digital'
   end
 
-  scenario 'user able to fill a contact form' do
+  scenario 'user able to find a contact form' do
     visit ('/')
     first('.fusion-main-menu').click_link('CONTACT US')
     expect(page).to have_content 'GET IN TOUCH'
+  end
+
+  scenario 'user able to fill in a contact form' do
+    visit ('/')
+    first('.fusion-main-menu').click_link('CONTACT US')
+    fill_in 'Name', with: 'j.Bloggs'
+    fill_in 'Email', with: 'j.Bloggs@qaworks.com'
+    fill_in 'Subject/Company/Project', with: 'test automation'
+    fill_in 'How can we help you?', with: 'please contact me I want to find out more'
+    expect(page).to have_field('Name', with: 'j.Bloggs')
+    expect(page).to have_field('Email', with: 'j.Bloggs@qaworks.com')
+    expect(page).to have_field('Subject/Company/Project', with: 'test automation')
+    expect(page).to have_field('How can we help you?', with: 'please contact me I want to find out more')
   end
 end
