1d804ac2f122aefdabd103f7dacfd9dc947c76a6
chuk-chuk <yulia.v.litvin@gmail.com>
2017-10-11 17:49:30 +0100

    scenario - loads home page content

--- spec/features/contact_us_page_spec.rb
+++ spec/features/contact_us_page_spec.rb
@@ -1,6 +1,11 @@
 feature 'contact us page' do
-  scenario 'it loads the page' do
+  scenario 'it loads home page' do
     visit ('/')
     expect(page.current_path).to eq('/')
+  end
+
+  scenario 'user can see content on the home page' do
+    visit ('/')
+    expect(page).to have_content 'QAWorks have been acquired by ECS Digital'
   end
 end
