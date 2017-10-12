7fb7d8e3bb9d31e68807e72d9869d201a407c696
chuk-chuk <yulia.v.litvin@gmail.com>
2017-10-12 01:06:41 +0100

    Specify the form for input

--- spec/features/contact_us_page_spec.rb
+++ spec/features/contact_us_page_spec.rb
@@ -16,46 +16,49 @@ feature 'contact us page' do
 
   scenario 'user able to fill in a contact form' do
     go_to_contact_page
-    fill_in_form
-    expect(page).to have_field('Name', with: 'j.Bloggs')
-    expect(page).to have_field('Email', with: 'j.Bloggs@qaworks.com')
-    expect(page).to have_field('Subject/Company/Project', with: 'test automation')
-    expect(page).to have_field('How can we help you?', with: 'please contact me I want to find out more')
+    within('form[id=qaworks-contact-us]') do
+      fill_in_form
+      expect(page).to have_field('Name', with: 'j.Bloggs')
+      expect(page).to have_field('Email', with: 'j.Bloggs@qaworks.com')
+      expect(page).to have_field('Subject/Company/Project', with: 'test automation')
+      expect(page).to have_field('How can we help you?', with: 'please contact me I want to find out more')
+    end
   end
 
   scenario 'user can submit a contact form' do
     go_to_contact_page
-    fill_in_form
-    click_button('send')
-    sleep 5
-    expect(page).to have_content 'THANK YOU FOR YOUR MESSAGE.'
+    within('form[id=qaworks-contact-us]') do
+      fill_in_form
+      click_button('send')
+      sleep 5
+      expect(page).to have_content 'THANK YOU FOR YOUR MESSAGE.'
+    end
   end
 
   scenario 'user can not leave empty fields' do
     go_to_contact_page
-    fill_in 'Name', with: ''
-    fill_in 'Email', with: ''
-    fill_in 'Subject/Company/Project', with: 'This is not a required field'
-    fill_in 'How can we help you?', with: ''
-    click_button('send')
-    sleep 5
-    within('form') do
+    within('form[id=qaworks-contact-us]') do
+        fill_in 'Name', with: ''
+        fill_in 'Email', with: ''
+        fill_in 'Subject/Company/Project', with: 'This is not a required field'
+        fill_in 'How can we help you?', with: ''
+        click_button('send')
+        sleep 5
         expect(page).to have_content('The field is required.', count: 3)
+        expect(page).to have_content 'PLEASE RE-SUBMIT WITH CORRECT INFORMATION'
     end
-    expect(page).to have_content 'PLEASE RE-SUBMIT WITH CORRECT INFORMATION'
   end
 
   scenario 'user can not fill in invalid email' do
     go_to_contact_page
-    fill_in 'Name', with: 'j.Bloggs'
-    fill_in 'Email', with: 'invalidEmail'
-    fill_in 'Subject/Company/Project', with: 'test automation'
-    fill_in 'How can we help you?', with: 'please contact me I want to find out more'
-    click_button('send')
-    sleep 5
-    within('form') do
-        expect(page).to have_content 'The e-mail address entered is invalid'
+    within('form[id=qaworks-contact-us]') do
+      fill_in 'Name', with: 'j.Bloggs'
+      fill_in 'Email', with: 'invalidEmail'
+      fill_in 'Subject/Company/Project', with: 'test automation'
+      fill_in 'How can we help you?', with: 'please contact me I want to find out more'
+      click_button('send')
+      sleep 5
+      expect(page).to have_content 'The e-mail address entered is invalid'
     end
-    expect(page).to have_content 'PLEASE RE-SUBMIT WITH CORRECT INFORMATION'
   end
 end
